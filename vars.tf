97ca7e1c284fc10c5caa737106400a94d26fd2f9
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-10 21:52:27 +0530

    roboshop - ansible

--- vars.tf
+++ vars.tf
@@ -2,4 +2,7 @@ variable "cidr_block" {}
 variable "tags" {}
 variable "env" {}
 variable "subnets" {}
-#variable "subnet_ids" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_vpc_rtid" {}
+
