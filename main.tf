a47fa793e845607fcef42a096a99811fe8ab5858
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-10 16:27:56 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -18,3 +18,9 @@ module "subnets" {
   tags = var.tags
   env  = var.env
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
+}
